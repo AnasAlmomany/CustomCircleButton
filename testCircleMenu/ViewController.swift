aaa2bee7f007e8296f477edc61295a0922bc2760
Anas Almomany <a.almomany@posrocket.com>
2017-12-20 10:21:49 +0200

    Dragable view

--- testCircleMenu/ViewController.swift
+++ testCircleMenu/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circleMenu: CustomCircleButton!
+    
+    var panGesture       = UIPanGestureRecognizer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.draggedView(_:)))
+        circleMenu.isUserInteractionEnabled = true
+        circleMenu.addGestureRecognizer(panGesture)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @objc func draggedView(_ sender:UIPanGestureRecognizer){
+        self.view.bringSubview(toFront: circleMenu)
+        let translation = sender.translation(in: self.view)
+        circleMenu.center = CGPoint(x: circleMenu.center.x + translation.x, y: circleMenu.center.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: self.view)
+    }
 
 }
 
